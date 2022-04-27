080f57a9a8624e1d30c6aa6968b2766d53c23d3d
Benjamin Lafever <BLafever@tql.com>
2022-04-27 11:11:09 -0400

    initial commit

---
+++ OdeToFood.data/IRestaurantData.cs
@@ -0,0 +1,18 @@
+﻿using OdeToFood.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OdeToFood.data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetRestaurantsByName(string name);
+        Restaurant GetById(int id);
+        Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
+        Restaurant Delete(int id);
+        int Commit();
+    }
+}
---
+++ OdeToFood.data/OdeToFoodDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OdeToFood.Core;
+
+namespace OdeToFood.data
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public OdeToFoodDbContext(DbContextOptions<OdeToFoodDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
